50dbc05acda8f296a0ea127d254be78260355f7f
MyronFanQiu <qiufan_glamour@hotmail.com>
2023-06-27 18:07:35 +0800

    add pos

--- pallets/ai/src/lib.rs
+++ pallets/ai/src/lib.rs
@@ -88,8 +88,8 @@ pub mod pallet {
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::call_index(0)]
-		#[pallet::weight(1000000000)]
-		pub fn ask(origin: OriginFor<T>,  prompt: Vec<u8>) -> DispatchResult {
+		#[pallet::weight(100000000000)]
+		pub fn ask(origin: OriginFor<T>,  prompt: Vec<u8>) -> DispatchResultWithPostInfo {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
 			// https://docs.substrate.io/main-docs/build/origins/
@@ -102,19 +102,19 @@ pub mod pallet {
 			Self::deposit_event(Event::Ask { who, latest_nonce, prompt: truncate_prompt });
 			<LastestNonce<T>>::put(latest_nonce + 1);
 			// Return a successful DispatchResultWithPostInfo
-			Ok(())
+			Ok(().into())
 		}
 
 		/// An example dispatchable that may throw a custom error.
 		#[pallet::call_index(1)]
-		#[pallet::weight(1000000000)]
-		pub fn reply(origin: OriginFor<T>, nonce: u64, answer: Vec<u8>) -> DispatchResult {
+		#[pallet::weight(100000000000)]
+		pub fn reply(origin: OriginFor<T>, nonce: u64, answer: Vec<u8>) -> DispatchResultWithPostInfo {
 			let who = ensure_signed(origin)?;
 			ensure!(Self::relayers(who), Error::<T>::MustBeRelayer);
 			let truncate_answer = BoundedVec::<u8, ConstU32<81920>>::truncate_from(answer.clone());
 			<ReplyRecords<T>>::insert(nonce, truncate_answer.clone());
 			Self::deposit_event(Event::Reply {nonce, answer: truncate_answer});
-			Ok(())
+			Ok(().into())
 		}
 
 		#[pallet::call_index(2)]
